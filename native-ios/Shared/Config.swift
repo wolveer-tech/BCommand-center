2e8ae5f92cb7dcb32e66f887c2eb7605d01baaf6
wolveer-tech <wolvesgidaree@gmail.com>
2026-09-02 15:40:22 +0100

    Delete native-ios/Shared/Config.swift

--- native-ios/Shared/Config.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum AppConfig {
-    static let commandCentreURL = URL(string: "https://bcommand-center.wolvesgidaree.workers.dev")!
-    static let stunServers = ["stun:stun.l.google.com:19302"]
-    static let appGroup = "group.tech.wolveer.commandcentre"
-    static let broadcastExtensionBundleID = "tech.wolveer.commandcentre.native.broadcast"
-}
